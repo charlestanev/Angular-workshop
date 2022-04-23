a74873d14ce723196c5e1900a1faef207165fc89
ELMIR TANEV <51314768+charlestanev@users.noreply.github.com>
2022-04-23 15:08:44 +0300

    moving all the services into the core section
    moving all the services into the core section

--- WORKSHOP-01/src/app/post-list/post-list.component.ts
+++ WORKSHOP-01/src/app/post-list/post-list.component.ts
@@ -1,6 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { IPost } from '../interfaces';
-import { PostService } from '../post.service';
+import { PostService } from '../core/post.service';
 
 @Component({
 	selector: 'app-post-list',
