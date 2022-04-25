16157a0c43c8e3710d9658c07065654b84b44d97
ELMIR TANEV <51314768+charlestanev@users.noreply.github.com>
2022-04-25 12:48:41 +0300

    init
    init

--- WORKSHOP-01/src/app/app.module.ts
+++ WORKSHOP-01/src/app/app.module.ts
@@ -5,26 +5,18 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './core/header/header.component';
 import { FooterComponent } from './core/footer/footer.component';
-import { ThemeListComponent } from './theme-list/theme-list.component';
-import { AsideComponent } from './aside/aside.component';
-import { ThemeListItemComponent } from './theme-list-item/theme-list-item.component';
-import { PostListComponent } from './post-list/post-list.component';
 import { CoreModule } from './core/core.module';
+import { ThemesModule } from './feature/themes/themes.module';
 
 @NgModule({
 	declarations: [
-		AppComponent,
-		// HeaderComponent,
-		// FooterComponent,
-		ThemeListComponent,
-		AsideComponent,
-		ThemeListItemComponent,
-		PostListComponent
+		AppComponent
 	],
 	imports: [
 		BrowserModule,
 		HttpClientModule,
-		CoreModule
+		CoreModule,
+		ThemesModule
 	],
 	providers: [
 
