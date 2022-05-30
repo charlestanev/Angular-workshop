2250a2987035236a34baf7265f8c658e96b59ba5
ELMIR TANEV <51314768+charlestanev@users.noreply.github.com>
2022-05-30 15:11:42 +0300

    init
    init

---
+++ WORKSHOP-01/src/app/shared/welcome/test.php
@@ -0,0 +1,52 @@
+<li class="dropdown open">
+	<a href="" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="true">
+		Intake & Consents
+	</a>
+	<ul class="dropdown-menu">
+		<li class="dropdown test open">
+			<a href="" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="true">
+				Inner Intake & Consents
+			</a>
+			<ul class="dropdown-menu ">
+				<li>
+					<a href="">Inner Inner Intake & Consents</a>
+				</li>
+				<li>
+					<a href="">Inner Inner Intake & Consents</a>
+				</li>
+			</ul>
+		</li>
+		<li class="dropdown test open">
+			<a href="" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="true">
+				Inner Intake & Consents
+			</a>
+			<ul class="dropdown-menu ">
+				<li>
+					<a href="">Inner Inner Intake & Consents</a>
+				</li>
+				<li>
+					<a href="">Inner Inner Intake & Consents</a>
+				</li>
+			</ul>
+		</li>
+		<li class="dropdown test open">
+			<a href="" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="true">
+				Inner Intake & Consents
+			</a>
+			<ul class="dropdown-menu ">
+				<li>
+					<a href="">Inner Inner Intake & Consents</a>
+				</li>
+				<li>
+					<a href="">Inner Inner Intake & Consents</a>
+				</li>
+			</ul>
+		</li>
+		<li>
+			<a href="">Inner Intake & Consents</a>
+		</li>
+		<li>
+			<a href="">Inner Intake & Consents</a>
+		</li>
+	</ul>
+</li>
